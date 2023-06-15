56683885c4ccaeb9234b10e51cce61323ba8b0d2
Fadipe Oladiran <fadipedav@gmail.com>
2023-06-15 08:47:09 -0700

    LLP in c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+ /**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("size of char:   %d bytes", sizeof(char));
+	printf("size of int:    %d bytes", sizeof(int));
+	printf("size of long int: %d bytes", sizeof(long int));
+	printf("size of float:  %d bytes", sizeof(float));
+	printf("size of long long int: %d bytes", sizeof(long long int));
+
+	return (0)
+}
