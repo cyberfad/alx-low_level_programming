0298bf922c1505fdf8a3ca4d642cd4cb80f798d0
Fadipe Oladiran <fadipedav@gmail.com>
2023-06-15 09:38:15 -0700

    a

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
-
  /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("size of char:   %d bytes", sizeof(char));
@@ -13,6 +11,5 @@ int main(void)
 	printf("size of long int: %d bytes", sizeof(long int));
 	printf("size of float:  %d bytes", sizeof(float));
 	printf("size of long long int: %d bytes", sizeof(long long int));
-
-	return (0)
+	return (0);
 }
