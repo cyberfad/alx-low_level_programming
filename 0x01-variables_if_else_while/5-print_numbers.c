2b256e778db74cba5bd11a61d3f116381c577713
Fadipe Oladiran <fadipedav@gmail.com>
2023-06-16 08:43:08 -0700

    printing single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - printing single digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 1; number++)
+		printf(number);
+	printf("\n");
+	return (0);
+}
