100c9be01b90fa01507607ef2ca9e1b3e9972051
Fadipe Oladiran <fadipedav@gmail.com>
2023-06-16 07:46:44 -0700

    printing all characters of the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+	/* use a for loop to iterate a through z */
+	for (alphabet = 'a'; alphabet <= 'z', alphabet++)
+		putchar(alphabet);
+	putchar('\n');
+	return (0);
+}
