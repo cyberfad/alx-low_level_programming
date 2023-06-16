a0bcd9aa48405b30f3a755e4a36d436fb6bccc48
Fadipe Oladiran <fadipedav@gmail.com>
2023-06-16 08:29:51 -0700

    printing alphabets selectively

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - printing alphabets in lower case without some letters
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet != 'q' && alphabet != e)
+			putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
