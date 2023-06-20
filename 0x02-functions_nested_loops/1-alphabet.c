17f56b9c2e3f4e51a7c1628f3ef07be91a0af8da
Fadipe Oladiran <fadipedav@gmail.com>
2023-06-20 09:28:43 -0700

    A

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
-
-	/**
-	 * print_alphabet - print all alphabet in lowercase
-	 */
+/**
+ * print_alphabet - print all alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
 
 
 	void print_alphabet(void)
