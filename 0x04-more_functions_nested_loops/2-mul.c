36d6bd090ad967b69c5cd05993fcead06212020e
Fadipe Oladiran <fadipedav@gmail.com>
2023-06-22 06:48:54 -0700

    a

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+# include <stdio.h>
+/**
+ * mul(int a, int b)-  multiplies two object
+ * @a: integer to be multiplied
+ * @b:  integers to multiply
+ * Return: 0 Success
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
