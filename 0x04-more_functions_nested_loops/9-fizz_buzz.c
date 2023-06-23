f190645bc9222903d103f61758d63306b871bccf
Fadipe Oladiran <fadipedav@gmail.com>
2023-06-23 08:37:34 -0700

    a

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/* A program that prints Fizz and Buzz*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if(n % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if(n % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else
+			printf("%d", n);
+	}
+    return 0;
+}
