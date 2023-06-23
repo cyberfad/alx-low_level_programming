cf7a77f158ece7f5da078f3ac62e12bcd6937fa8
Fadipe Oladiran <fadipedav@gmail.com>
2023-06-23 04:55:22 -0700

    a

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,14 +7,24 @@
 
 void print_diagonal(int n)
 {
-	int i;
-
-	for (i = 0; i < n; i++)
 	{
-		if (n <= 0)
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
 			_putchar('\n');
-		else
-			_putchar('\\');
+		}
 	}
-	_putchar('\n');
 }
